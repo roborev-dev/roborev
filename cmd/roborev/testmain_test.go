e00583814970d941ded91973212fae1ad862fe22
Wes McKinney <wesmckinn+git@gmail.com>
2026-02-18 17:13:30 -0600

    Refactor TestMain to isolate run logic and enable defer for cleanup

--- cmd/roborev/testmain_test.go
+++ cmd/roborev/testmain_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"testing"
 )
@@ -10,14 +11,19 @@ import (
 // git subprocess spawning). Without skipExternalIO, the 200+ tests that call
 // newTuiModel each spawn git subprocesses, exhausting macOS CI runner resources.
 func TestMain(m *testing.M) {
+	os.Exit(runTests(m))
+}
+
+func runTests(m *testing.M) int {
 	skipExternalIO = true
 
 	tmpDir, err := os.MkdirTemp("", "roborev-test-*")
 	if err != nil {
-		panic(err)
+		fmt.Fprintf(os.Stderr, "failed to create temp dir: %v\n", err)
+		return 1
 	}
+	defer os.RemoveAll(tmpDir)
+
 	os.Setenv("ROBOREV_DATA_DIR", tmpDir)
-	code := m.Run()
-	os.RemoveAll(tmpDir)
-	os.Exit(code)
+	return m.Run()
 }
