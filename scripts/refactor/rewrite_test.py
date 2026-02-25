da61da828c66966638d1761b0d0a425c257c2417
Wes McKinney <wesmckinn+git@gmail.com>
2026-02-25 07:05:34 -0600

    chore: remove refactor scripts

--- scripts/refactor/rewrite_test.py
+++
@@ -1,8 +0,0 @@
-import re
-
-def main():
-    with open('cmd/roborev/review_test.go', 'r') as f:
-        content = f.read()
-
-    # We will write the new content directly instead of complex regex replaces
-    # Let's just create a new file content.
--- scripts/refactor/test_refactor.py
+++
@@ -1,6 +0,0 @@
-import re
-
-with open("internal/storage/sync_test.go", "r") as f:
-    content = f.read()
-
-# I will write the replacement logic here
