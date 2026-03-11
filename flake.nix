1e4c1d56a38a58867bc0783284fee0bb4823e10e
Wes McKinney <wesmckinn+git@gmail.com>
2026-03-11 07:48:34 -0500

    Update flake.nix for v0.46.0

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
         packages = {
           default = pkgs.buildGoModule {
             pname = "roborev";
-            version = "0.45.0";
+            version = "0.46.0";
 
             src = ./.;
 
